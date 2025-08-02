485fdb76428508d8210a6316fb99806b3e0f6ce9
Anuga Senithu De Silva <anjanee.uragaha@gmail.com>
2025-08-02 22:10:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Put your session id here",
+SESSION_ID: process.env.SESSION_ID || "VISPER-MD&mox0ASzL#OPxEZzuOpRrXGgEMvwlBm9YbxJTUoVZ8jw2C0yPISow",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://postimg.cc/nX6ZH38b",
 ALIVE_MSG: process.env.ALIVE_MSG || "HELLO I AM CYBER ANUWH MDX V1 MADE BY JAVA SCRIPT BY CYBER ANUWH &I AM ALIVE NOW 😀😀<NOW ALIVE> ",
 };
